38ca7833e3a5a4ccea1d025ac88583d1d230804d
Md. Fahim Shahriar Adnan <fahimadnan5@gmail.com>
2023-11-20 00:23:39 +0600

    home ui design added

---
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -0,0 +1,25 @@
+<?php namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+
+class HomeController extends Controller
+{
+
+
+    public function index()
+    {
+
+        return view('frontend.home');
+    }
+
+}
