172329847a2486bfab16ac15d7a48887489be324
Md. Fahim Shahriar Adnan <fahimadnan5@gmail.com>
2023-11-22 01:10:14 +0600

    dynamic properties sell item added

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
+use App\Models\Property;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
@@ -18,8 +19,16 @@ class HomeController extends Controller
 
     public function index()
     {
+        $properties=Property::all();
 
-        return view('frontend.home');
+        return view('frontend.home',compact('properties'));
+    }
+
+    public function store(Request $request)
+    {
+        $data= Property::create($request->all());
+
+        return redirect()->back();
     }
 
     public function cart()
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -3,7 +3,28 @@
 @section('content')
     <div class="container px-4 px-lg-5 mt-5">
         <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-            <div class="col mb-5">
+            @foreach ($properties as $item)
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" src="https://www.nsapropertiesltd.com/wp-content/uploads/2019/12/Front-view.jpg" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">{{  $item->title }} </h5>
+                                <!-- Product price-->
+                                {{  $item->price }}
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            {{-- <div class="col mb-5">
                 <div class="card h-100">
                     <!-- Product image-->
                     <img class="card-img-top" src="https://www.nsapropertiesltd.com/wp-content/uploads/2019/12/Front-view.jpg" alt="..." />
@@ -198,9 +219,9 @@
                         <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Call</a></div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
-    
+
     @include('frontend.layouts.includes.modal.sell-property-modal')
 @endsection
