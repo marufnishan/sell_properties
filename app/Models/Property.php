f3f1be0e9288dec0def822120b4f6ef54852d4b8
Md. Fahim Shahriar Adnan <fahimadnan5@gmail.com>
2023-11-22 01:09:32 +0600

    properties table and model file added

---
+++ app/Models/Property.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Property extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function approver()
+    {
+        return $this->belongsTo(User::class, 'approved_by');
+    }
+}
